39ea317a5220fa27e6dca05476e3c4dc08170514
mikio-korean22 <mikio.fullmoon.korea@gmail.com>
2020-10-30 03:27:16 +0000

    post

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
     before_save {self.email.downcase! }
     
     has_secure_password
+    has_many :microposts
     
     validates :name, presence: true, length: {maximum: 50}
     validates :email, presence: true, length: {maximum: 255},
